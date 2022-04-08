3f33132f950c4d19abb3764eeafd36dbefbaccee
Precup Eduard <precupeduard99@gmail.com>
2022-04-09 01:02:31 +0300

    Update project - Styling and making section-products hide/display at a click on services-link

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # OSF-Academy-eCommerce-Frontend-Project
+-header done
+-navbar done
+-section products hide/display on click on services-link done
+
+task - all links should redirect to 404 page created by design provided in section 8.1. 404 Page.
+task - make the footer
+task - try to make breakpoint in media querries for a responsive layout
---
+++ js/script.js
@@ -0,0 +1,36 @@
+const servicesProducts = [
+    'Accesories', 'Alcohol', 'Art', 'Books', 'Drink', 'Electronics', 'Flowers & Plants', 'Food', 'Gadgets', 'Garden', 'Grocery', 'Home', 
+    'Jewelry', 'Kids & Baby', "Men's fashion", 'Mobile', 'Motorcyles', 'Movies', 'Music', 'Office', 'Pets', 'Romantic', 'Sport', 'Toys'
+]
+
+const saleServicesProducts = [
+    'Accesories', 'Alcohol', 'Art', 'Books', 'Drink', 'Electronics', 'Flowers & Plants', 'Food'
+]
+
+
+const productsList = document.querySelector('.products-list')
+const saleProductsList = document.querySelector('.products-sale-list')
+
+// Rendering all product items in the html
+for (let i = 0; i < servicesProducts.length; i++) {
+    productsList.innerHTML += `<li> <a href=""> ${servicesProducts[i]} </a> </li>`
+}
+
+for (let i = 0; i < saleServicesProducts.length; i++) {
+    saleProductsList.innerHTML += `<li> <a href=""> ${saleServicesProducts[i]} </a> </li>`
+}
+
+
+
+// Making the section products hide/show when user click on services-link
+// When user click services-link style it 
+const $btnServicesLinkNavbar = $('#services-navbar')
+const $sectionServicesProducts = $('.section-services-products')
+
+$sectionServicesProducts.hide()
+
+$btnServicesLinkNavbar.on('click', ()=>{
+    $btnServicesLinkNavbar.toggleClass('style-services-link')
+    $sectionServicesProducts.toggle(700)
+})
+
