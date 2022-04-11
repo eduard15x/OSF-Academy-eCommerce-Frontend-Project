e620b970a01fbe5dd8fe537b1da023cf86603776
Precup Eduard <precupeduard99@gmail.com>
2022-04-11 21:08:32 +0300

    Made the 404-page, link all li from project

--- js/script.js
+++ js/script.js
@@ -8,18 +8,17 @@ const saleServicesProducts = [
 ]
 
 
-
-
 const productsList = document.querySelector('.products-list')
 const saleProductsList = document.querySelector('.products-sale-list')
 
 // Rendering all product items in the html
 for (let i = 0; i < servicesProducts.length; i++) {
-    productsList.innerHTML += `<li> <a href=""> ${servicesProducts[i]} </a> </li>`
+    productsList.innerHTML += `<li class="li-product-services"> <a href="#non-product-404-page"> ${servicesProducts[i]} </a> </li>`
 }
 for (let i = 0; i < saleServicesProducts.length; i++) {
-    saleProductsList.innerHTML += `<li> <a href=""> ${saleServicesProducts[i]} </a> </li>`
+    saleProductsList.innerHTML += `<li class="li-product-services"> <a href="#non-product-404-page"> ${saleServicesProducts[i]} </a> </li>`
 }
+
 
 
 
@@ -39,41 +38,42 @@ $btnServicesLinkNavbar.on('click', ()=>{
 
 //Inject current year in the footer section
 let currentYear = new Date().getFullYear()
-
 const $spanYearFooter = $('#year-footer')
-
 $spanYearFooter.text(currentYear)
 
 
 
 //FORM LOGIN SECTION
-
 //Adding an event when click on user-login icon from navbar open the  Login pop-up
 const $formLoginSection = $('.form-login-box')
 const $userLoginIcon = $('#user-login-register');
 
-$formLoginSection.hide()
-
-$userLoginIcon.on('click', ()=> {
-    $formLoginSection.fadeIn(800)
-})
-
-//When user click "forget password" -> delete password from input 
-//When user click on the eye from input, show the password
-
 const $formInputPassword = $('#form-input-password')
 const $forgetPassword = $('#forget-password')
 const $togglePassword = $('#togglePassword')
+const $createAccount = $('#create-account')
+const $formRegisterSection = $('.form-create-account-box')
+//selector for form and register form
+const modal = document.querySelector('.modal')
+
+$formLoginSection.hide()
+$formRegisterSection.hide()
+
+$userLoginIcon.on('click', ()=> {
+    $formLoginSection.show()
+    modal.style.display = 'block'
+    $formRegisterSection.hide()
+    $('#oops').css('zIndex', 0)
+})
 
 
+//When user click "forget password" -> delete password from input 
+//When user click on the eye from input, show the password
 $forgetPassword.on('click', ()=> {
     $formInputPassword.val('')
 })
 
-
-
 //DONT KNOW WHY THIS IS NOT WORKING !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
 // $togglePassword.click(function() {
 //     if( type === 'password') {
 //         $formInputPassword.attr('type', 'text')
@@ -93,52 +93,52 @@ $togglePassword.click(function(){
     }
 })
 
-
-
-
-
-
 //Making the p with no account displayng a new register form when clicked
-const $createAccount = $('#create-account')
-const $formRegisterSection = $('.form-create-account-box')
-
-$formRegisterSection.hide()
-
 $createAccount.click(()=>{
     $formLoginSection.hide()
-    $formRegisterSection.fadeIn(500)
+    $formRegisterSection.show()
 })
 
+//Making the modal to be displayed in the background and the login/register form to be hidden when click on overlay
+window.addEventListener('click', clickCloseModal)
+window.addEventListener('keydown', keypressCloseModal)
 
-
-
-
-
-
-
-//CLOSE MODALS WHEN CLICK OUTSIDE OF THEM !!!!!!!!!!HELP
-// const $formsModal = $('.modal')
-
-// function closeModel() {
-//     $('html').on('click', (e)=> {
-//         if ( $(e.target).hasClass('modal') ) {
-//             $formsModal.show()
-//             if ( !$(e.target).hasClass('modal') ) {
-//                 $formsModal.hide()
-                
-//             }   
-//         } 
-//     })    
-// }
-
-
-
-document.addEventListener("click", function(event) {
-    if ( !event.target.closest(".modal") ) {
-        closeModal()
+function clickCloseModal(e) {
+    if ( e.target === modal ) {
+        $formLoginSection.hide()
+        modal.style.display = 'none'
+        $('#oops').css('zIndex', 1)
     }
-})
-  
-  function closeModal() {
-    document.querySelector(".modal").style.display = "none"
-  }
+}
+
+function keypressCloseModal(e) {
+    if ( e.key === 'Escape') {
+        $formLoginSection.hide()
+        modal.style.display = 'none'
+        $('#oops').css('zIndex', 1)
+    }
+}
+
+
+
+
+const $nonProductSection = $('.non-product-404')
+$nonProductSection.hide()
+
+
+window.addEventListener("click",function(e) {
+    if (e.target.getAttribute('href') == "#non-product-404-page") {
+      console.log('da')
+      $nonProductSection.show()
+      $sectionServicesProducts.hide()
+      $btnServicesLinkNavbar.removeClass('style-services-link')
+    } else if ( e.target.getAttribute('href') == "#home") {
+        $nonProductSection.hide()
+    }
+});
+
+
+
+
+const $links = $('li a')
+$links.attr('href', '#non-product-404-page')
